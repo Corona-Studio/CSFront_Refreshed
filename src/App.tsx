eea7fec3ce019e652373b4703e346eb446f5ed7e
laolarou <luolinrui726@outlook.com>
2025-02-15 00:10:03 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.css";
 
-import { useEffect } from "react";
+import { createRef, CSSProperties, useEffect, useState } from "react";
 import { Outlet } from "react-router";
+import { BackTop } from "tdesign-react";
 
 import MenuBar from "./components/MenuBar.tsx";
 import { useThemeDetector } from "./helpers/ThemeDetector.ts";
 
 function App() {
+    const containerRef = createRef<HTMLDivElement>();
     const themeDetector = useThemeDetector();
+
+    const style: CSSProperties = {
+        position: "absolute",
+        insetInlineEnd: 24,
+        insetBlockEnd: 80,
+        zIndex: 10000
+    };
 
     useEffect(() => {
         if (themeDetector) {
@@ -20,10 +29,17 @@ function App() {
 
     return (
         <>
-            <div className="w-full h-screen">
+            <div className="w-full h-screen" ref={containerRef}>
                 <MenuBar />
-                <Outlet />
+                <div style={{ position: "relative" }}>
+                    <Outlet />
+                </div>
             </div>
+
+            <BackTop
+                container={() => containerRef.current!}
+                visibleHeight={46}
+                style={style}></BackTop>
         </>
     );
 }
