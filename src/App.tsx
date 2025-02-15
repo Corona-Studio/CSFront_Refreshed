e09f9c71b1950d6bc3181ff4bb234f09a678c3b6
laolarou <luolinrui726@outlook.com>
2025-02-14 23:05:27 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import "./App.css";
-
-import { BrowserRouter, Route, Routes } from "react-router";
-
 import MenuBar from "./components/MenuBar.tsx";
-import Index from "./pages/Index.tsx";
-import LxIndex from "./pages/LauncherX/Index.tsx";
 
 function App() {
     return (
