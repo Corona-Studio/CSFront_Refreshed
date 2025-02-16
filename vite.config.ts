5b812748f58d7f1da66599676bd87fa3b50ce9f7
laolarou <luolinrui726@outlook.com>
2025-02-15 23:50:11 -0800

    use zstd

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+import { compress } from "@mongodb-js/zstd";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { patchCssModules } from "vite-css-modules";
+import { compression } from "vite-plugin-compression2";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [patchCssModules(), react(), tailwindcss()]
+    plugins: [
+        patchCssModules(),
+        react(),
+        tailwindcss(),
+        compression<{ level: number }>({
+            algorithm(buf, opt) {
+                return compress(buf as never, opt.level);
+            },
+            compressionOptions: {
+                level: 9
+            },
+            filename: "[path][base].zst"
+        })
+    ]
 });
