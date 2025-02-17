0f4aabb61d6bb66a09511dc3a8a793f4173b030f
laolarou <luolinrui726@outlook.com>
2025-02-16 22:57:27 -0800

    Update UserLogin.tsx

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -10,7 +10,7 @@ function UserLogin() {
     return (
         <>
             <div className="p-8 space-y-4 bg-zinc-50/30 dark:bg-zinc-900/80 bg-opacity-25 rounded-2xl hover:shadow-lg active:shadow-md shadow transition">
-                <h4>{t("login")}</h4>
+                <h5>{t("login")}</h5>
                 <Form className="w-[300px] md:w-[400px] lg:w-[450px]" statusIcon={true} colon={true} labelWidth={0}>
                     <FormItem name="account">
                         <Input clearable={true} prefixIcon={<DesktopIcon />} placeholder={t("pleaseInputEmail")} />
