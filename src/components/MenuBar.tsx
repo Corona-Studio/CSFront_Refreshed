206f6e4d84d963fab1c0d7bc60b4dd41abbe247f
laolarou <luolinrui726@outlook.com>
2025-02-16 17:07:25 -0800

    修复标题栏在移动端的显示问题

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router";
-import { LinkIcon, MoonIcon, SunnyIcon } from "tdesign-icons-react";
-import { Button, MenuValue } from "tdesign-react";
+import { LinkIcon, MoonIcon, SunnyIcon, ViewListIcon } from "tdesign-icons-react";
+import { Button, Dropdown, DropdownOption, MenuValue } from "tdesign-react";
 import HeadMenu from "tdesign-react/es/menu/HeadMenu";
 import MenuItem from "tdesign-react/es/menu/MenuItem";
 
@@ -12,6 +12,11 @@ import styles from "./MenuBar.module.css";
 
 const t = i18next.t;
 
+interface MenuItemValue {
+    to: string;
+    isInSiteLink: boolean;
+}
+
 function MenuBar() {
     const themeDetector = useThemeDetector();
     const [active, setActive] = useState<MenuValue>("0");
@@ -20,14 +25,59 @@ function MenuBar() {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const linkOptions: DropdownOption[] = [
+        {
+            content: t("indexPage"),
+            value: { to: "/", isInSiteLink: true }
+        },
+        {
+            content: "LauncherX",
+            value: { to: "/lx", isInSiteLink: true }
+        },
+        {
+            content: "CMFS",
+            value: { to: "/cmfs", isInSiteLink: true }
+        },
+        {
+            content: t("cskb"),
+            value: { to: "https://kb.corona.studio/", isInSiteLink: false }
+        },
+        {
+            content: t("moreProjects"),
+            value: { to: "https://github.com/Corona-Studio/", isInSiteLink: false }
+        }
+    ];
+
+    function onMenuItemClicked(dropdownItem: DropdownOption) {
+        if (!dropdownItem.value) return;
+
+        const value = dropdownItem.value as MenuItemValue;
+
+        if (value.isInSiteLink) {
+            navigate(value.to);
+            return;
+        }
+
+        window.open(value.to, "_blank");
+    }
+
     const operations = () => (
-        <div className="pr-8">
+        <div className="pr-8 flex">
             <Button
                 variant="text"
                 shape="square"
                 icon={isDarkMode ? <MoonIcon /> : <SunnyIcon />}
                 onClick={switchTheme}
             />
+            <Dropdown
+                direction="right"
+                hideAfterItemClick={true}
+                placement="bottom"
+                trigger="hover"
+                options={linkOptions}
+                onClick={onMenuItemClicked}>
+                <Button variant="text" shape="square" icon={<ViewListIcon />} />
+            </Dropdown>
         </div>
     );
 
@@ -59,24 +109,38 @@ function MenuBar() {
                     theme="light"
                     value={active}
                     onChange={(v) => setActive(v)}
-                    logo={<img className={styles.menuLogo} src={logo} height="28" alt="logo" onClick={onLogoClicked} />}
+                    logo={<img className={styles.menuLogo} src={logo} alt="logo" onClick={onLogoClicked} />}
                     operations={operations()}>
-                    <MenuItem value={"/"} onClick={() => navigate("/")}>
+                    <MenuItem value={"/"} onClick={() => navigate("/")} className="invisible w-0 md:visible md:w-auto">
                         <span>{t("indexPage")}</span>
                     </MenuItem>
-                    <MenuItem value={"/lx"} onClick={() => navigate("/lx")}>
+                    <MenuItem
+                        value={"/lx"}
+                        onClick={() => navigate("/lx")}
+                        className="invisible w-0 md:visible md:w-auto">
                         <span>LauncherX</span>
                     </MenuItem>
-                    <MenuItem value={"/cmfs"} onClick={() => navigate("/cmfs")}>
+                    <MenuItem
+                        value={"/cmfs"}
+                        onClick={() => navigate("/cmfs")}
+                        className="invisible w-0 md:visible md:w-auto">
                         <span>CMFS</span>
                     </MenuItem>
-                    <MenuItem value={"cskb"} href="https://kb.corona.studio/" target="_blank">
+                    <MenuItem
+                        value={"cskb"}
+                        href="https://kb.corona.studio/"
+                        target="_blank"
+                        className="invisible w-0 md:visible md:w-auto">
                         <span>
                             {t("cskb")}
                             <LinkIcon />
                         </span>
                     </MenuItem>
-                    <MenuItem value={"moreProj"} href="https://github.com/Corona-Studio/" target="_blank">
+                    <MenuItem
+                        value={"moreProj"}
+                        href="https://github.com/Corona-Studio/"
+                        target="_blank"
+                        className="invisible w-0 md:visible md:w-auto">
                         <span>
                             {t("moreProjects")}
                             <LinkIcon />
