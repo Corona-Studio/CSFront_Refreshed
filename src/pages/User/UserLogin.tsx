ee883b047041af71c69f67cfa6b7821730ccde4c
laolarou <luolinrui726@outlook.com>
2025-02-16 22:28:48 -0800

    优化登陆表单 UI

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -9,7 +9,7 @@ const t = i18next.t;
 function UserLogin() {
     return (
         <>
-            <div className="p-8 space-y-4 bg-zinc-50 dark:bg-zinc-900 bg-opacity-25 rounded-2xl hover:shadow-lg active:shadow-md shadow transition">
+            <div className="p-8 space-y-4 bg-zinc-50/30 bg-opacity-20 dark:bg-zinc-900 bg-opacity-25 rounded-2xl hover:shadow-lg active:shadow-md shadow transition">
                 <h4>{t("login")}</h4>
                 <Form className="w-[300px] md:w-[400px] lg:w-[450px]" statusIcon={true} colon={true} labelWidth={0}>
                     <FormItem name="account">
