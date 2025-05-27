4a30f0143e726c5925c5eb4e326a7c5d6cb893b3
laolarou <luo.linr@northeastern.edu>
2025-05-26 17:46:15 -0700

    Update StorageHelper.ts

--- src/helpers/StorageHelper.ts
+++ src/helpers/StorageHelper.ts
@@ -1,8 +1,5 @@
 import localForage from "localforage";
 
-import { StoredAuthToken } from "../requests/LxAuthRequests.ts";
-import { checkUserIsPaidAsync } from "../requests/LxUserRequests.ts";
-
 export const I18NLangKey = "I18N_LANG_KEY";
 
 export async function getStorageItemAsync(key: string) {
